d80916787e88410190593499e7007e9605d84e8f
Thomas Brandstätter <brand.vienna@gmail.com>
2018-04-28 22:32:44 +0200

    - add date_calculator

--- header/lang/driver.h
+++ header/lang/driver.h
@@ -11,8 +11,69 @@ namespace lang {
 
     class driver {
     public:
+
+        /**
+         * ease the way we calculate with dates
+         */
+        struct date_calculator {
+
+            const long kMillisecondPerSecond;
+            const long kSecondPerMinute;
+            const long kMinutePerHour;
+            const long kHourPerDay;
+
+            static date_calculator& shared_instance() {
+                static date_calculator dc;
+                return dc;
+            }
+
+
+            inline double
+            days_in_ms(const double value)
+            {
+                return
+                    value
+                    * kHourPerDay
+                    * kMinutePerHour
+                    * kSecondPerMinute
+                    * kMillisecondPerSecond;
+            }
+
+            inline double
+            hours_in_ms(const double value) const
+            {
+                return
+                    value
+                    * kMinutePerHour
+                    * kSecondPerMinute
+                    * kMillisecondPerSecond;
+            }
+
+            inline double
+            minutes_in_ms(const double value) const
+            {
+                return value * kSecondPerMinute * kMillisecondPerSecond;
+            }
+
+            inline double
+            seconds_in_ms(const double value) const
+            {
+                return value * kSecondPerMinute;
+            }
+
+            date_calculator()
+                :    kMillisecondPerSecond(1000)
+                ,kSecondPerMinute(60)
+                ,kMinutePerHour(60)
+                ,kHourPerDay(24)
+            {}
+        };
+
+        date_calculator dc;
+
         driver() :
-            _debug(true)
+            _debug(true),
+            dc()
         {
         }
 

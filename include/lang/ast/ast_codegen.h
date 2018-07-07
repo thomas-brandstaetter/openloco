54a17e78d9fcd44093c4a4ff310b833c631e908a
Thomas Brandstätter <thomas@brandstifter.io>
2018-07-07 10:39:11 +0200

    - add ast interface for code generation

---
+++ include/lang/ast/ast_codegen.h
@@ -0,0 +1,25 @@
+#pragma once
+/**
+ * @author Thomas Brandstätter
+ * @date 07.07.18.
+ */
+
+#include <llvm/IR/Value.h>
+
+
+class ast_codegen
+{
+public:
+    virtual ~ast_codegen() {}
+
+    /**
+     * codegen
+     *
+     * AST objects are used to build llvm::Value objects. This method handles the transformation from AST to LLVM IL.
+     * A value is used to build IR code by llvm.
+     *
+     * @see http://llvm.org/doxygen/classllvm_1_1Value.html
+     * @return llvm::Value
+     */
+    virtual std::unique_ptr<llvm::Value> codegen() = 0;
+};
